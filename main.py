5ecd95652c573cd2e546a607b2d869a4a024a02d
Anteeeeh <antoine.ehrhart2002@gmail.com>
2026-04-19 12:21:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,38 @@
 import os
+import pandas as pd
+import yfinance as yf
 import requests
 import time
 
 TOKEN = os.getenv("TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
-if not TOKEN or not CHAT_ID:
-    print("❌ TOKEN ou CHAT_ID manquant")
-    exit()
-
 def send(msg):
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
     requests.post(url, data={"chat_id": CHAT_ID, "text": msg})
 
-print("✅ BOT DÉMARRÉ")
+def analyze():
+    df = pd.read_csv("portfolio.csv")
 
-send("🚀 BOT CONNECTÉ AVEC SUCCÈS")
+    for _, row in df.iterrows():
+        data = yf.download(row["ticker"], period="1d", interval="1h")
+
+        if data.empty:
+            continue
+
+        price = float(data["Close"].iloc[-1])
+        change = ((price - row["pru"]) / row["pru"]) * 100
+
+        if change > 5:
+            signal = "🚀 OPPORTUNITÉ (RENFORCER)"
+            send(f"{row['asset']} → {signal}\nPerf: {round(change,2)}%")
+
+        elif change < -5:
+            signal = "⚠️ RISQUE (SURVEILLER / VENDRE)"
+            send(f"{row['asset']} → {signal}\nPerf: {round(change,2)}%")
+
+print("✅ BOT ANALYSE LANCÉ")
 
 while True:
-    send("⏰ BOT ACTIF - TEST HEURE")
+    analyze()
     time.sleep(3600)
