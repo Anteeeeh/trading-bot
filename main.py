5913ddb10782f269ce5ae3a701ca9102759d8151
Anteeeeh <antoine.ehrhart2002@gmail.com>
2026-04-19 12:17:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,21 @@ import os
 import requests
 import time
 
-TOKEN = os.environ["TOKEN"]
-CHAT_ID = os.environ["CHAT_ID"]
+TOKEN = os.getenv("TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+if not TOKEN or not CHAT_ID:
+    print("❌ TOKEN ou CHAT_ID manquant")
+    exit()
 
 def send(msg):
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
     requests.post(url, data={"chat_id": CHAT_ID, "text": msg})
 
-# message test au démarrage
+print("✅ BOT DÉMARRÉ")
+
 send("🚀 BOT CONNECTÉ AVEC SUCCÈS")
 
-# boucle toutes les heures
 while True:
     send("⏰ BOT ACTIF - TEST HEURE")
     time.sleep(3600)
